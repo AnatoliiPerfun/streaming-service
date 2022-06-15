30a616394f60bfcc223d1c58279f5d95d6c26744
AnatoliiPerfun <tolikvik@gmail.com>
2022-06-15 19:30:21 +0300

    generator implementation + simple JUnit test added

---
+++ src/main/java/com/example/streamingservice/service/QuoteGeneratorService.java
@@ -0,0 +1,11 @@
+package com.example.streamingservice.service;
+
+import com.example.streamingservice.model.Quote;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public interface QuoteGeneratorService {
+
+    Flux<Quote> fetchQuoteStream(Duration period);
+}
---
+++ src/test/java/com/example/streamingservice/service/QuoteGeneratorImplTest.java
@@ -0,0 +1,37 @@
+package com.example.streamingservice.service;
+
+import com.example.streamingservice.model.Quote;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuoteGeneratorImplTest {
+
+    QuoteGeneratorService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new QuoteGeneratorImpl();
+    }
+
+    @Test
+    void fetchQuoteStream() throws InterruptedException {
+        Flux<Quote> quotesFlux = service.fetchQuoteStream(Duration.ofMillis(100L));
+
+        Consumer<Quote> quoteConsumer = System.out::println;
+        Consumer<Throwable> throwableConsumer = e -> System.out.println(e.getMessage());
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        Runnable done = countDownLatch::countDown;
+
+        quotesFlux.take(30)
+                .subscribe(quoteConsumer, throwableConsumer, done);
+        countDownLatch.await();
+    }
+}
